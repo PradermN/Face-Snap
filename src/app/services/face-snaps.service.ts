94ee655dd2bc555a2fecc9ea7b8827a616f7024f
PradermN <PradermN@example.com>
2023-10-13 16:56:41 +0200

    update photos

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -8,6 +8,14 @@ export class FaceSnapsService {
     faceSnaps: FaceSnap[] = [
         {
             id: 1,
+            title: 'Water, my element',
+            description: 'Nature',
+            imageUrl: 'assets/paysage1.jpg',
+            createdDate: new Date(),
+            snaps: 374
+        },
+        {
+            id: 2,
             title: 'Choupidou',
             description: 'My best friend since I was little',
             imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
@@ -16,7 +24,7 @@ export class FaceSnapsService {
             location: 'paris'
         },
         {
-            id: 2,
+            id: 3,
             title: 'Three Rock Mountain',
             description: 'A very nice place for hiking.',
             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
@@ -25,13 +33,22 @@ export class FaceSnapsService {
             location: 'The mountain'
         },
         {
-            id: 3,
-            title: 'Un bon repas',
-            description: 'Mmmh que c\'est bon !',
+            id: 4,
+            title: 'A good and healthy meal',
+            description: 'Mmmh it is so tasty!',
             imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
             createdDate: new Date(),
             snaps: 101
+        },
+        {
+            id: 5,
+            title: 'Afrcan women',
+            description: 'Culture',
+            imageUrl: 'assets/portrait5.jpg',
+            createdDate: new Date(),
+            snaps: 500
         }
+        
     ]
 
     getAllFaceSnaps(): FaceSnap[]{
